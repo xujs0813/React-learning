3d8912d259ac5c23c00ce5b11e155295c219abbd
xujinshan <xujinshan1@kingsoft.com>
2020-09-16 09:38:53 +0800

    simple react-router-demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/users'>Users</Link></li>
+        </ul>
+      </div>
+      <Switch>
+        <Route path='/about'><About></About></Route>
+        <Route path='/users'><Users></Users></Route>
+        <Route path='/'><Home></Home></Route>
+      </Switch>
+    </Router>
   );
+}
+
+function Home(){
+  return <div>I am Home</div>
+}
+
+function About(){
+  return <div>I am About</div>
+}
+
+
+function Users(){
+  return <div>I am Users</div>
 }
 
 export default App;
